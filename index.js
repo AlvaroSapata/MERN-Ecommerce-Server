1c9add33862100a08f94e117898bcd53f8d7a8d0
Alvaro Martinez <alvaromfdv@gmail.com>
2024-03-21 10:30:00 +0100

    first commit - conection with DB

---
+++ index.js
@@ -0,0 +1,25 @@
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+// Database Connection With MongoDB
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Ecommerce";
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    const dbName = x.connections[0].name;
+    console.log(`Connected to Mongo! Database name: "${dbName}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
