8b891b600a78e20853c272fd2bb4489b63d01e01
Alvaro Martinez <alvaromfdv@gmail.com>
2024-03-21 11:10:37 +0100

    Added Image Storage Engine - Multer

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = 4000;
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
+
+// ℹ️ package responsible to store images
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
@@ -28,63 +30,91 @@ mongoose
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
   });
-  
-  //* ------------------   Models   ------------------ *//
-  // ℹ️ Schema for creating user model
-  const Users = mongoose.model("Users", {
-    name: {
-      type: String,
-    },
-    email: {
-      type: String,
-      unique: true,
-    },
-    password: {
-      type: String,
-    },
-    cartData: {
-      type: Object,
-    },
-    date: {
-      type: Date,
-      default: Date.now,
-    },
-  });
-  
-  // ℹ️ Schema for creating Product
-  const Product = mongoose.model("Product", {
-    id: {
-      type: Number,
-      required: true,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    image: {
-      type: String,
-      required: true,
-    },
-    category: {
-      type: String,
-      required: true,
-    },
-    new_price: {
-      type: Number
-    },
-    old_price: {
-      type: Number
-    },
-    date: {
-      type: Date,
-      default: Date.now,
-    },
-    avilable: {
-      type: Boolean,
-      default: true,
-    },
-  });
 
+//* ------------------   Models   ------------------ *//
+// ℹ️ Schema for creating user model
+const Users = mongoose.model("Users", {
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  cartData: {
+    type: Object,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// ℹ️ Schema for creating Product
+const Product = mongoose.model("Product", {
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number,
+  },
+  old_price: {
+    type: Number,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  avilable: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+//* ---------------   Image Storage Engine   --------------- *//
+// ℹ️ Multer configuration for storing images
+const storage = multer.diskStorage({
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    console.log(file);
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+// ℹ️ Multer route for uploading images
+const upload = multer({ storage: storage });
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:4000/images/${req.file.filename}`,
+  });
+});
+app.use("/images", express.static("upload/images"));
+
+//* ------------------   Middlewares   ------------------ *//
+
+//* ------------------   Routes   ------------------ *//
+
+// ℹ️ Route for creating new user
 
 app.listen(port, (error) => {
   if (!error) console.log("Server Running on port " + port);
