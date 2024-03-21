e5e8e6ed6887b03e1d9233b07f2892e539c6ba39
Alvaro Martinez <alvaromfdv@gmail.com>
2024-03-21 11:33:27 +0100

    Added middleware, auth routes and Readme

--- index.js
+++ index.js
@@ -111,10 +111,96 @@ app.post("/upload", upload.single("product"), (req, res) => {
 app.use("/images", express.static("upload/images"));
 
 //* ------------------   Middlewares   ------------------ *//
+// ℹ️ MiddleWare to fetch user from database
+const fetchuser = async (req, res, next) => {
+  const token = req.header("auth-token");
+  if (!token) {
+    res.status(401).send({ errors: "Please authenticate using a valid token" });
+  }
+  try {
+    const data = jwt.verify(token, "secret_ecom");
+    req.user = data.user;
+    next();
+  } catch (error) {
+    res.status(401).send({ errors: "Please authenticate using a valid token" });
+  }
+};
 
 //* ------------------   Routes   ------------------ *//
+// Root route
+app.get("/", (req, res) => {
+  res.send("Root");
+});
+// Authentication Routes
+// ℹ️ Create an endpoint at ip/login for login the user and giving auth-token
+app.post("/login", async (req, res) => {
+  console.log("Login");
+  let success = false;
+  let user = await Users.findOne({ email: req.body.email });
+  if (user) {
+    const passCompare = req.body.password === user.password;
+    if (passCompare) {
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      success = true;
+      console.log(user.id);
+      const token = jwt.sign(data, "secret_ecom");
+      res.json({ success, token });
+    } else {
+      return res
+        .status(400)
+        .json({
+          success: success,
+          errors: "please try with correct email/password",
+        });
+    }
+  } else {
+    return res
+      .status(400)
+      .json({
+        success: success,
+        errors: "please try with correct email/password",
+      });
+  }
+});
 
-// ℹ️ Route for creating new user
+// ℹ️ Create an endpoint at ip/auth for regestring the user in data base & sending token
+app.post("/signup", async (req, res) => {
+  console.log("Sign Up");
+  let success = false;
+  let check = await Users.findOne({ email: req.body.email });
+  if (check) {
+    return res
+      .status(400)
+      .json({
+        success: success,
+        errors: "existing user found with this email",
+      });
+  }
+  let cart = {};
+  for (let i = 0; i < 300; i++) {
+    cart[i] = 0;
+  }
+  const user = new Users({
+    name: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    cartData: cart,
+  });
+  await user.save();
+  const data = {
+    user: {
+      id: user.id,
+    },
+  };
+
+  const token = jwt.sign(data, "secret_ecom");
+  success = true;
+  res.json({ success, token });
+});
 
 app.listen(port, (error) => {
   if (!error) console.log("Server Running on port " + port);
